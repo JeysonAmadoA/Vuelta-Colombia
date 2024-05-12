de31db702b14783c7a1e9730cdd774e0d5699040
JeysonAmado <jeysonamado97@hotmail.com>
2024-05-11 21:32:18 -0500

    Creación de proyecto

---
+++ src/main/java/com/uptc/frmw/VueltaColombiaApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueltaColombiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueltaColombiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frmw/VueltaColombiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frmw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueltaColombiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
